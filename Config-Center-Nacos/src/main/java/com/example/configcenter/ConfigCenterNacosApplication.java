bd29c7f11c48dac7bb78bcc1887e729f24f26d74
morty <fc1303678317@gmail.com>
2023-02-01 22:03:14 +0800

    Init

---
+++ Config-Center-Nacos/src/main/java/com/example/configcenter/ConfigCenterNacosApplication.java
@@ -0,0 +1,13 @@
+package com.example.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCenterNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterNacosApplication.class, args);
+    }
+
+}
